c3d640d966ec918d4391303f5c53b12998a54eac
Bruno Cabral <cabral.brc@gmail.com>
2022-11-10 12:07:13 -0800

    split data dev and train

--- main.py
+++ main.py
@@ -17,3 +17,13 @@ m, n = data.shape
 np.random.shuffle(data)  # avoid overfitting
 
 # %%
+data_dev = data[0:1000].T  # transpose to make math easier
+Y_dev = data_dev[0]
+X_dev = data_dev[1:n]
+
+# %%
+data_train = data[1000:].T  # transpose to make math easier
+Y_train = data_train[0]
+X_train = data_train[1:n]
+
+# %%
