63899683c2b60bd902dbc18c772a4b9e68217153
Bruno Cabral <cabral.brc@gmail.com>
2022-11-10 12:11:19 -0800

    init_params Generate weights and biases

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # %%
-data = pd.read_csv('./mnist/mnist_train.csv')
+data = pd.read_csv("./mnist/mnist_train.csv")
 
 # %%
 data.head()
@@ -26,4 +26,12 @@ data_train = data[1000:].T  # transpose to make math easier
 Y_train = data_train[0]
 X_train = data_train[1:n]
 
+
 # %%
+def init_params():
+    '''Generate weights and biases'''
+    W1 = np.random.randn(10, 784) - 0.5
+    b1 = np.random.randn(10, 1) - 0.5
+    W2 = np.random.randn(10, 10) - 0.5
+    b2 = np.random.randn(10, 1) - 0.5
+    return W1, b1, W2, b2
