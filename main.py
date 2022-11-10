1312fe9e1fb2bec4ef478027fe837789c87127bf
Bruno Cabral <cabral.brc@gmail.com>
2022-11-10 11:59:00 -0800

    read train

---
+++ main.py
@@ -0,0 +1,15 @@
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+data = pd.read_csv('./mnist/mnist_train.csv')
+
+# %%
+data.head()
+
+# %%
+data = np.array(data)
+
+# %%
